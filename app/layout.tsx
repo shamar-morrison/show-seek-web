c9f4378b5e36798619cf66f74ebcb9a7da9bae78
Shamar Morrison <shamar.webdev@gmail.com>
2026-01-04 22:11:12 -0500

    feat: Replace Geist fonts with Inter font and add `--text-xs` CSS variable.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
 import { Footer } from "@/components/footer"
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+import { Inter } from "next/font/google"
 import NextTopLoader from "nextjs-toploader"
 import { Toaster } from "sonner"
 import "./globals.css"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 })
 
@@ -27,9 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} antialiased font-sans`}>
         <NextTopLoader color="#E50914" showSpinner={false} />
         {children}
         <Footer />
