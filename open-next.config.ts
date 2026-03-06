db841f47c8acccbd0185cf99897dde70a10f6391
Shamar Morrison <shamar.webdev@gmail.com>
2026-03-06 11:05:58 -0500

    refactor: migrate incremental cache from Cloudflare R2 to KV.

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,8 +1,8 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare"
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache"
+import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache"
 import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue"
 
 export default defineCloudflareConfig({
-  incrementalCache: r2IncrementalCache,
+  incrementalCache: kvIncrementalCache,
   queue: doQueue,
 })
